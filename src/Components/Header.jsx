f29124ffb0144390ec399e07114800cd80f0cdb1
Hammii123 <hammadurrehman957@gmail.com>
2026-09-04 18:28:44 +0500

    fix: correct import case for SearchBar component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import SearchBar from "./Searchbar";
+import SearchBar from "./SearchBar";
 import SortDropdown from "./SortDropdown";
 
 function Header({ searchTerm, onSearchChange, sortKey, sortOptions, onSortChange }) {
